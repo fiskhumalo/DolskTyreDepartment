2a0e9e66196dea3f31984a54f65f1c320e8a74e9
fiskhumalo <fiskhumalo@gmail.com>
2026-06-19 22:11:39 +0200

    chore: remove dead code (CartItemRepository, ReviewRepository, empty util)

--- src/main/java/com/dolsk/tyres/repository/CartItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dolsk.tyres.repository;
-
-import com.dolsk.tyres.model.CartItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-}
